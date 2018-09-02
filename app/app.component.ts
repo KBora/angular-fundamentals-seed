c50b60d1958d5aa621ec00ba741acc4e2c2a7485
wookkii <wookkii@home>
2018-09-02 14:14:56 +1000

    Add Child inteface - can reference Child interface in the Passenger interface

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+interface Child {
+  name: string,
+  age: number
+}
+
 interface Passenger {
   id: number,
   fullname: string,
   checkedIn: boolean,
-  checkedInDate: number | null
+  checkedInDate: number | null,
+  children: Child[] | null
 }
 
 @Component({
@@ -38,25 +44,29 @@ export class AppComponent {
     id: 1,
     fullname: 'Stephen',
     checkedIn: true,
-    checkedInDate: 1490742000000
+    checkedInDate: 1490742000000,
+    children: null
   },
   {
     id: 2,
     fullname: 'Rose',
     checkedIn: false,
-    checkedInDate: null
+    checkedInDate: null,
+    children: [{ name: 'Jessica', age: 11}, { name: 'Ted', age: 3}]
   },
   {
     id: 3,
     fullname: 'Bobby',
     checkedIn: true,
-    checkedInDate: 1491606000000
+    checkedInDate: 1491606000000,
+    children: null
   },
   {
     id: 4,
     fullname: 'Vishnu',
     checkedIn: true,
-    checkedInDate: 1488412800000
+    checkedInDate: 1488412800000,
+    children: [{ name: 'Helen', age: 5} ]
   }
   ]
 }
