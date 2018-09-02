c171dcddb9bcc7fb4a0a562ecfc53de9bfa791b5
wookkii <wookkii@home>
2018-09-03 08:49:09 +1000

    Add PassengerDashboardComponent to PassengerDashboardModule in declarations

--- app/app.component.ts
+++ app/app.component.ts
@@ -17,28 +17,8 @@ interface Passenger {
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
-    <div class="app">
-      <h3>Airline Passengers</h3>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index;">
-          <span 
-            class="status"
-            [class.checked-in]="passenger.checkedIn"
-            >
-          </span>
-            {{ i }}: {{ passenger.fullname }}
-          <p>{{ passenger | json }} </p>
-          <div class="date">
-            Check in date: 
-            {{ passenger.checkedIn ? (passenger.checkedInDate | date: 'yMMMMd' | uppercase )  : 'Not checked in' }}
-          </div> 
-          <div class="children">
-            Children: {{ passenger.children?.length || 0 }}
-          </div>
-        </li>
-      </ul>
-  
-
+    <div class="app">  
+      <passenger-dashboard></passenger-dashboard>
     </div>   
   `
 })
