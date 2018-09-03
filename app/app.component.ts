09854cf9e1020dd6f35248f7b0c7b170bd48526e
wookkii <wookkii@home>
2018-09-03 22:39:14 +1000

    Add Passenger data by copying across array

--- app/app.component.ts
+++ app/app.component.ts
@@ -23,33 +23,4 @@ interface Passenger {
   `
 })
 export class AppComponent {
-  passengers: Passenger[] = [ {
-    id: 1,
-    fullname: 'Stephen',
-    checkedIn: true,
-    checkedInDate: 1490742000000,
-    children: null
-  },
-  {
-    id: 2,
-    fullname: 'Rose',
-    checkedIn: false,
-    checkedInDate: null,
-    children: [{ name: 'Jessica', age: 11}, { name: 'Ted', age: 3}]
-  },
-  {
-    id: 3,
-    fullname: 'Bobby',
-    checkedIn: true,
-    checkedInDate: 1491606000000,
-    children: null
-  },
-  {
-    id: 4,
-    fullname: 'Vishnu',
-    checkedIn: true,
-    checkedInDate: 1488412800000,
-    children: [{ name: 'Helen', age: 5} ]
-  }
-  ]
 }
