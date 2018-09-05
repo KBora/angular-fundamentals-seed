18d79bd28628d16f5af6e852ead76f6557e11bb1
wookkii <wookkii@home>
2018-09-05 22:32:06 +1000

    Create service via dependency injection

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
+import { PassengerDashboardService } from '../../passenger-dashboard.service'
+
 import { Passenger } from '../../models/passenger.interface';
 
 @Component({
@@ -26,39 +29,14 @@ import { Passenger } from '../../models/passenger.interface';
 
 export class PassengerDashboardComponent  implements OnInit {
     passengers: Passenger[];
-    constructor() {}
+
+    constructor(private passengerService: PassengerDashboardService) {}
+
     ngOnInit() {
-        console.log('ngOnInit');
-        this.passengers = [ {
-            id: 1,
-            fullname: 'Stephen',
-            checkedIn: true,
-            checkedInDate: 1490742000000,
-            children: null
-          },
-          {
-            id: 2,
-            fullname: 'Rose',
-            checkedIn: false,
-            checkedInDate: null,
-            children: [{ name: 'Jessica', age: 11}, { name: 'Ted', age: 3}]
-          },
-          {
-            id: 3,
-            fullname: 'Bobby',
-            checkedIn: true,
-            checkedInDate: 1491606000000,
-            children: null
-          },
-          {
-            id: 4,
-            fullname: 'Vishnu',
-            checkedIn: true,
-            checkedInDate: 1488412800000,
-            children: [{ name: 'Helen', age: 5} ]
-          }
-          ]
+        console.log('PassengerDashboardComponent: ngOnInit');
+        this.passengers = this.passengerService.getPassengers();
     }
+
     handleEdit(event: Passenger) {
       console.log(event);
 
