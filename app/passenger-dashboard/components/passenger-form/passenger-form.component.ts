df5a5ff801a6b84c4bb97dcbc070f5bd19cd6361
wookkii <wookkii@home>
2018-09-09 10:48:17 +1000

    Form validation - create ref, specifty required = true, display error via json

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input} from '@angular/core';
 
 import { Passenger } from '../../models/passenger.interface';
+import { Baggage } from '../../models/baggage.interface';
 
 @Component({
     selector: 'passenger-form',
@@ -13,16 +14,25 @@ import { Passenger } from '../../models/passenger.interface';
                 Passenger name:
                 <input 
                     type="text"
-                    name="fullname"                    
+                    name="fullname"
+                    required="true"
+                    #fullname="ngModel"                    
                     [ngModel]="detail?.fullname">
+                {{ fullname.errors | json }}
             </div>
             <div>
                 Passenger ID:
                 <input 
                     type="number"
-                    name="id"                    
+                    name="id"     
+                    required="true"
+                    #id="ngModel"              
                     [ngModel]="detail?.id">
+                {{ id.errors | json }}
+                
             </div> 
+
+            
 
             <div>
                 <label>
@@ -33,7 +43,6 @@ import { Passenger } from '../../models/passenger.interface';
                         (ngModelChange)="toggleCheckIn($event)">
                     Yes
                 </label>
-
             </div>
 
      
@@ -45,6 +54,21 @@ import { Passenger } from '../../models/passenger.interface';
                     [ngModel]="detail?.checkedInDate">
             </div>
            
+            <div>
+                Luggage:
+                <select
+                    name="baggage"
+                    [ngModel]="detail?.baggage">
+                    <option
+                        *ngFor="let item of baggage"
+                        [value]="item.key"
+                        [selected]="item.key === detail?.baggage">
+                        {{ item.value }}
+                    </option>
+                </select>
+
+            </div>
+
             {{ passengerForm.value | json }}       
 
            </form>
@@ -54,6 +78,20 @@ import { Passenger } from '../../models/passenger.interface';
 export class PassengerFormComponent {
     @Input()
     detail: Passenger;
+
+    baggage: Baggage[] = [{
+        key: 'none',
+        value: 'No baggage'
+    },{
+        key: 'hand-only',
+        value: 'Hand baggage'
+    },{
+        key: 'hold-only',
+        value: 'Hold baggage'
+    },{
+        key: 'hand-hold',
+        value: 'Hand and hold baggage'
+    }];
 
     toggleCheckIn(checkedIn: boolean) {
         if (checkedIn) {
