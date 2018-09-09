03d1a0e930e628f37a2d21b97158d61534d30beb
wookkii <wookkii@home>
2018-09-09 20:26:20 +1000

    Setup routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 
+import { HomeComponent } from './home.component';
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full'},
+  
+];
 
 @NgModule({
   imports: [
     // angular modules
     BrowserModule,
+    RouterModule.forRoot(routes),
     // custom modules
     PassengerDashboardModule
   ],
@@ -16,7 +24,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ]
 })
 export class AppModule {}
