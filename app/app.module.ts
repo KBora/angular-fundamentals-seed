8f097f901efcd25318c4207a3c7e5cc5be84009f
wookkii <wookkii@home>
2018-09-09 20:29:30 +1000

    Add wildcard routing to implement not found page

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,11 +5,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 
 import { HomeComponent } from './home.component';
+import { NotFoundComponent } from './not-found.component';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full'},
-  
+  { path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
@@ -25,7 +26,8 @@ const routes: Routes = [
   ],
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NotFoundComponent
   ]
 })
 export class AppModule {}
